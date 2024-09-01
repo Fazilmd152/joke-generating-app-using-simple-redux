1eaf8c934789d6204a2746431c05266d392824df
Fazilmd152 <mohamedfazil152@gmail.com>
2024-09-02 05:23:50 +0530

    Spelling corrected

--- src/Components/aboutApp.jsx
+++ src/Components/aboutApp.jsx
@@ -4,7 +4,7 @@ import styles from '../styles'
 const AboutApp = () => {
   return (
     <div className={styles.aboutApp}>
-        <h1 className={styles.aboutAppH1}>Random joke generating App using chuk noris api</h1>
+        <h1 className={styles.aboutAppH1}>Random joke generating App using chuck norris api</h1>
     </div>
   )
 }
